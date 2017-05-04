ea856933e8a9e3613fcf198ba1a649887b668cf2
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-05-04 20:47:38 +0300

    Collecting Data with Forms and Validation. Editing Data with Two-way Bindings.

--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core'
-import { Router } from '@angular/router'
+import {Component, OnInit} from '@angular/core'
+import {Router} from '@angular/router'
 import {EventService} from './shared/index'
 
 @Component({
@@ -13,10 +13,28 @@ import {EventService} from './shared/index'
         .error :-ms-input-placeholder {color: #999;}
     `]
 })
-export class CreateEventComponent{
+export class CreateEventComponent implements OnInit {
     isDirty:boolean = true;
+    event:any;
+
     constructor(private router: Router, private eventService: EventService){
 
+    }
+
+    ngOnInit(){
+        this.event = {
+            name: 'Ng Spectacular',
+            date: '8/8/2028',
+            time: '10am',
+            price: 799.99,
+            location: {
+                address: '456 Happy St',
+                city: 'Felicity',
+                country: 'Angularistan'
+            },
+            onlineUrl: 'http://ngSpectacular.com',
+            imageUrl: 'http://ngSpectacular.com/logo.png'
+        }
     }
 
     saveEvent(formValues){
