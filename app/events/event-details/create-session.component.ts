d7750750f3ec8fd34c82f286ac5fa9d83e45defa
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-05-07 13:41:30 +0300

    Collecting Data with Forms and Validation. Creating Custom Validators.

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
-import {ISession} from '../shared/index'
+import {ISession, restrictedWords} from '../shared/index'
 
 @Component({
     templateUrl: 'app/events/event-details/create-session.component.html',
@@ -23,11 +23,11 @@ export class CreateSessionComponent implements OnInit {
     abstract: FormControl
 
     ngOnInit(){
-        this.name = new FormControl('', Validators.required)
-        this.presenter = new FormControl('', Validators.required)
-        this.duration = new FormControl('', Validators.required)
-        this.level = new FormControl('', Validators.required)
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+        this.name = new FormControl('', Validators.required);
+        this.presenter = new FormControl('', Validators.required);
+        this.duration = new FormControl('', Validators.required);
+        this.level = new FormControl('', Validators.required);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
