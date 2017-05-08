53b66aa0a9276cfac67dea55b3294e292ec33ffc
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-05-08 20:56:19 +0300

    Displaying Data with Pipes. Using Built-in Pipes.

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,25 +4,7 @@ import {IEvent} from './shared/index'
 
 @Component({
     selector: 'event-thumbnail',
-    template: `
-    <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-        <h2 class="main-class" [ngClass]="getStartTimeClass()">{{event?.name}}</h2>
-        <div [ngClass]="{green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}">Date: {{event?.date}}</div>
-        <div [class.green]="event?.time === '8:00 am'"
-                [ngSwitch]="event?.time">
-            Time: {{event?.time}}
-            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
-            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-            <span *ngSwitchDefault>(Normal Start)</span>
-        </div>
-        <div>Price: \${{event.price}}</div>
-        <div [hidden]="!event?.location">
-            <span [style.color]="event?.time === '8:00 am' ? '#003300' : '#bbb'">Location: {{event?.location?.address}}</span>
-            <span class="pad-left" style="font-size: medium" [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : '#bbb', 'font-weight': event?.time === '8:00 am' ? 'bold': 'normal'}">
-                {{event?.location?.city}}, {{event?.location?.country}}</span>
-        </div>
-    </div>
-    `,
+    templateUrl: 'app/events/event-thumbnail.component.html',
     styles: [`
         .green { color: #003300 !important; }
         .bold { font-weight: bold; }
